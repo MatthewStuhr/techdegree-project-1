fdb4426b7c868720cc6e884414bdf18eea67d672
Matthew Stuhr <MatthewStuhr@github.com>
2019-06-27 19:16:21 -0700

    Update script.js
    Added a tags object for category.

--- js/script.js
+++ js/script.js
@@ -11,55 +11,64 @@ let quotes = [
     source: 'Cersei Lannister'  ,
     quote:  'When you play the game of thrones, you win or you die.',
     citation: 'Game of Thrones: Season 1; Episode 7',
-    year: '2011'
+    year: '2011',
+    tags: 'Politics'
   },
   {
     source: 'Ned Stark' ,
     quote:  'The man who passes the sentence should swing the sword.',
     citation: 'Game of Thrones: Season 1; Episode 1',
-    year: '2011'
+    year: '2011',
+    tags: 'Leadership'
   },
   {
     source: 'Tyrion Lannister'  ,
     quote:  'Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you.',
     citation: 'Game of Thrones: Season 1; Episode 1',
-    year: '2011'
+    year: '2011',
+    tags: 'Motivational'
   },
   {
     source: 'Tywin Lannister'  ,
     quote:  'A lion does not concern himself with the opinion of sheep.',
     citation: 'Game of Thrones: Season 1; Episode 7',
-    year: '2011'
+    year: '2011',
+    tags: 'Politics'
   },
   {
     source: 'Lord Varys'  ,
     quote:  'Storms come and go,the big fish eat the little fish, and I keep on paddling.',
     citation: 'Game of Thrones: Season 2; Episode 2',
-    year: '2012'
+    year: '2012',
+    tags: 'Analogy'
   },
   {
     source: 'Petyr Baelish'  ,
     quote:  'The past is gone for good. You can sit here mourning its departure, or prepare for the future.',
     citation: 'Game of Thrones: Season 6; Episode 10',
-    year: '2016'
+    year: '2016',
+    tags: 'Motivational'
   },
   {
     source: 'Samwell Tarly'  ,
     quote:  'I was nothing at all. And when you are nothing at all, there is no reason to be afraid.',
     citation: 'Game of Thrones: Season 4; Episode 9',
-    year: '2014'
+    year: '2014',
+    tags: 'Motivational'
   },
   {
     source: 'Jorah Mormont'  ,
     quote:  'No one can survive in this world without help. No one.',
     citation: 'Game of Thrones: Season 2; Episode 7',
-    year: '2012'
+    year: '2012',
+    tags: 'Fact'
   },
   {
     source: 'Jon Snow'  ,
     quote:  'I dun want it.',
     citation: 'Game of Thrones: Season 8; Episode 1, 2, 3, 4, and 5',
-    year: '2019'
+    year: '2019',
+    tags: 'Politics'
   }
 ];
 // console.log(quotes);
@@ -110,13 +119,17 @@ function printQuote() {
     html += '<p class="quote">' + printRandomQuote.quote + '</p>';
  //displays the source from the html file.
     html += '<p class="source">' + printRandomQuote.source;
-//conditional statements whether the quote has a citation or year or both.
+//conditional statements whether the quote has a citation, year, or tags.
   if ("citation" in printRandomQuote) {
     html += '<span class="citation">' + printRandomQuote.citation + '</span>';
   }
 
   if ("year" in printRandomQuote) {
-    html += '<span class="year">' + printRandomQuote.year + '</span>';
+    html += '<span class="year">' + printRandomQuote.year + '</span> ';
+  }
+
+  if ("tags" in printRandomQuote) {
+    html += '<span class="tags">' + printRandomQuote.tags + '</span>';
   }
     html += '</p>';
  //displays html content
